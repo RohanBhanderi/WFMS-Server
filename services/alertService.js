554b733ec52ba4150a7832cf714269940d0c5724
komalkishorvohra <komalkvohra@gmail.com>
2015-05-05 14:24:55 -0700

    publish alert and add/list guard changes

--- services/alertService.js
+++ services/alertService.js
@@ -37,8 +37,17 @@ exports.handle_request = function(msg,callback){
 		case "alertPerClient":
 			alertPerClient(message,callback);
 			break;
+			
+		case "activeAdminAlerts":
+			activeAdminAlerts(message,callback);
+			break;
+			
 		case "seenByClient":
 			seenByClient(message,callback);
+			break;
+		
+		case "seenByAdmin":
+			seenByAdmin(message,callback);
 			break;
 			
 		default : 
@@ -324,6 +333,16 @@ function seenByAdmin(msg,callback){
 }
 
 
-
+function activeAdminAlerts(msg,callback){
+	mysql.queryDb('select alertinfo.*, building.buildingname, building.idclient,guard.idperson, client.idperson, personGuard.fname as guard_fname,personGuard.lname as guard_lname, personclient.fname as client_fname, personclient.lname as client_lname from alertinfo left join	building on alertinfo.idbuilding = building.idbuilding left join guard on alertinfo.idguard = guard.idguard	left join person as personGuard on guard.idperson = personGuard.idperson left join client on building.idclient = client.idclient left join person as personclient on client.idperson = personclient.idperson where status="F" ',function(err,rows){
+		if (err) {
+			console.log("Error while listing all the guard details !!!"  + err);
+			callback({ status : 500, message : "Error while listing guard details !!!" });
+		} else {
+			callback({ status : 200, data : rows});
+		}
+		
+	});
+};
 
 // Save updateClientBillingInfo
